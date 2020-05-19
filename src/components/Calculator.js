2f2037561b85482e947c32610fab9f8e60dee655
Trend20 <enockomondi305@gmail.com>
2020-05-19 14:55:57 -0400

    values

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,6 +20,22 @@ class Calculator extends Component {
                   <Button onClick={this.handleClick} label="4" value="4" />
                   <Button onClick={this.handleClick} label="1" value="1" />
                   <Button onClick={this.handleClick} label="0" value="0" />
+
+                  <Button onClick={this.handleClick} label="/" value="/" />
+                  <Button onClick={this.handleClick} label="8" value="8" />
+                  <Button onClick={this.handleClick} label="5" value="5" />
+                  <Button onClick={this.handleClick} label="2" value="2" />
+                  <Button onClick={this.handleClick} label="." value="." />
+
+                  <Button onClick={this.handleClick} label="x" value="*" />
+                  <Button onClick={this.handleClick} label="9" value="9" />
+                  <Button onClick={this.handleClick} label="6" value="6" />
+                  <Button onClick={this.handleClick} label="3" value="3" />
+                  <Button label="" value="null" />
+
+                  <Button onClick={this.handleClick} label="-" value="-" />
+                  <Button onClick={this.handleClick} label="+" size="2" value="+" />
+                  <Button onClick={this.handleClick} label="=" size="2" value="equal" />
               </Buttons>
             </div>
         );
