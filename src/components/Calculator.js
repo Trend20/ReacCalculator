241e0ee445362b52faf56e7b6032ad99f5ae6b3e
Trend20 <enockomondi305@gmail.com>
2020-05-19 14:52:48 -0400

    buttons commit

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Display from './Display';
+import Buttons from './Buttons';
+import Button from './Button';
  
 class Calculator extends Component {
     constructor(props){
@@ -10,7 +13,14 @@ class Calculator extends Component {
     render() { 
         return (
             <div>
-                
+              <Display data={this.state.operations} />
+              <Buttons>
+                  <Button onClick={this.handleClick} label="C" value="clear" />
+                  <Button onClick={this.handleClick} label="7" value="7" />
+                  <Button onClick={this.handleClick} label="4" value="4" />
+                  <Button onClick={this.handleClick} label="1" value="1" />
+                  <Button onClick={this.handleClick} label="0" value="0" />
+              </Buttons>
             </div>
         );
     }
