3743a12b76e5a2b6d7f27153adcfa07ba254fabc
Trend20 <enockomondi305@gmail.com>
2020-05-19 14:45:59 -0400

    components commit

---
+++ src/components/Calculator.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+ 
+class Calculator extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            operations:[],
+        }
+    }
+    render() { 
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+ 
+export default Calculator;
